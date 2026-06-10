b719b64c56ea9762414be11236907a1d7e170480
José Manuel Mármol Ávila <josemanuel.marmol@alu.ceacfp.es>
2026-06-10 13:46:45 +0200

    claves

---
+++ lib/firebase.ts
@@ -0,0 +1,34 @@
+import { initializeApp } from 'firebase/app';
+import type { Auth } from 'firebase/auth';
+import { getAuth, initializeAuth, onAuthStateChanged } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import { Platform } from 'react-native';
+
+const firebaseConfig = {
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+
+let auth: Auth;
+if (Platform.OS === 'web') {
+  auth = getAuth(app);
+} else {
+  const AsyncStorage = require('@react-native-async-storage/async-storage').default;
+  const { getReactNativePersistence } = require('firebase/auth') as typeof import('firebase/auth') & {
+    getReactNativePersistence: (storage: any) => any;
+  };
+  auth = initializeAuth(app, {
+    persistence: getReactNativePersistence(AsyncStorage),
+  });
+}
+
+export { auth };
+export const db = getFirestore(app);
+export const listenAuth = (callback: (user: any) => void) =>
+  onAuthStateChanged(auth, callback);
